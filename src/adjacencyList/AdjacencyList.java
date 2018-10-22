3c9811473beda50a69c1454e303f271f94cdc692
Tyler Herrmann <nemo2924@live.com>
2018-10-22 18:00:40 -0500

    Added BFS

--- src/adjacencyList/AdjacencyList.java
+++ src/adjacencyList/AdjacencyList.java
@@ -1,164 +1,245 @@
 package adjacencyList;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
-
+import java.io.FileNotFoundException;
+import java.io.File;
 
 public class AdjacencyList {
-	
-	// Creating linked list of the linked lists (Nodes)
-	private static LinkedList<edge> adjLists[];
-	
+
 	// Create an array of all the values within the text file named inputArray
 	// Values 0 - 3 in this list are as follows: 0 - numbers of vertices, 1 - number of edges, 
 	// 2 - Directed / undirected, 3 - Weighted / Unweighed
 	// All values following 3 are values for the graph.
-	static List<Double> inputArray = new ArrayList<>();
-	
-	// Creating the "Vertices" aka Linked Lists
-	private static void initializeGraph(int vertices) {
-		
-		adjLists = new LinkedList[vertices];
-		
-		for (int i = 0; i < vertices; i++) {
-			adjLists[i] = new LinkedList();
-		}	
-		
+	private static List<Double> inputArray = new ArrayList<>();
+
+    private static void addEdgeWeighted(Graph graph, int src, int dest, double weight) {
+
+        // Assigning the next value of every new edge to null by default
+        Edge next = null;
+
+        // Create a new edge using the passed in values and giving it a null next value
+        Edge newEdge = new Edge(dest, weight, next);
+
+        // If this edge is not the first then assign the previous edges next to the newly created edge.
+        if(!graph.adjListArray[src].isEmpty()) {
+            graph.adjListArray[src].getLast().next = newEdge;
+        }
+
+        // Add our edge to the list
+        graph.adjListArray[src].addFirst(newEdge);
+
+    }
+
+    private static void addEdgeUnweighted(Graph graph, int src, int dest) {
+
+        // Assigning the next value of every new edge to null by default
+        Edge next = null;
+
+        // Create a new edge using the passed in values and giving it a null next value and a 0 for weight
+        Edge newEdge = new Edge(dest, 0, next);
+
+        // If this edge is not the first then assign the previous edges next to the newly created edge.
+        if(!graph.adjListArray[src].isEmpty()) {
+            graph.adjListArray[src].getLast().next = newEdge;
+        }
+
+        // Add our edge to the list
+        graph.adjListArray[src].addFirst(newEdge);
+
+    }
+
+	private static void printGraph(Graph graph) {
+
+	    System.out.println("Print Adjacency List:\n");
+
+	    for(int i=0; i<graph.getNumberOfVertices();i++) {
+
+            System.out.print(i + ":");
+
+            for (AdjacencyList.Edge degree: graph.adjListArray[i]) {
+
+                System.out.print(" -> " + degree.vertex);
+                // If the graph is weighted, print out the weight with the vertex
+                if(graph.getIsWeighted()) {
+                    System.out.print(" (" + degree.weight + ")");
+                }
+
+            }
+
+            System.out.println("\n");
+        }
+
 	}
 	
-	private static void addEdgeWeighted(int src, int dest, double weight) {
+	// Printing a BFS style representation of our graph.
+	private static void BFS(Graph graph) {
 		
-		// Assigning the next value of every new edge to null by default
-		edge next = null;
+		int e;
 		
-		// Create a new edge using the passed in values and giving it a null next value
-		edge newEdge = new edge(dest, weight, next);
+		System.out.println("Print BFS:\n");
 		
-		// If this edge is not the first then assign the previous edges next to the newly created edge.
-		if(!adjLists[src].isEmpty()) {
-			adjLists[src].getLast().next = newEdge;
+		for(int i=0; i<graph.getNumberOfVertices();i++) {
+			
+			System.out.println("Process " + i + " early");
+			
+			for(int c=0;c<graph.adjListArray[i].size();c++) {
+				System.out.println("Process edge " + i + " " + graph.adjListArray[i].get(c).vertex);
+			}
+			
+			System.out.println("Process " + i + " late");
 		}
-		
-		// Add our edge to the list
-		adjLists[src].add(newEdge);
-		
 		
 	}
-	
-	private static void addEdgeUnweighted(int src, int dest) {
-		
-		// Assigning the next value of every new edge to null by default
-		edge next = null;
-		
-		// Create a new edge using the passed in values and giving it a null next value and a 0 for weight
-		edge newEdge = new edge(dest, 0, next);
-		
-		// If this edge is not the first then assign the previous edges next to the newly created edge.
-		if(!adjLists[src].isEmpty()) {
-			adjLists[src].getLast().next = newEdge;
+
+	private static class Graph {
+
+        private int numberOfVertices;
+        private int numberOfEdges;
+		private boolean isDirected;
+		private boolean isWeighted;
+
+        public LinkedList<AdjacencyList.Edge> adjListArray[];
+
+        public Graph(int v, int e) {
+            this.numberOfVertices = v;
+            this.numberOfEdges = e;
+            // Set directed and weighted to false automatically
+            this.isDirected = false;
+            this.isWeighted = false;
+            // Creating linked list of the linked lists (Nodes)
+            this.adjListArray = new LinkedList[this.numberOfVertices];
+
+            initializeGraph();
+        }
+
+        private void initializeGraph() {
+
+        	for(int i=0;i<this.numberOfVertices; i++) {
+                // Create a new list for each vertex
+        		this.adjListArray[i] = new LinkedList<>();
+			}
+
 		}
-		
-		// Add our edge to the list
-		adjLists[src].add(newEdge);
-		
+
+		public int getNumberOfVertices() {
+			return this.numberOfVertices;
+		}
+
+        public void setDirected(double directed) {
+            if(directed == 1) {
+                isDirected = true;
+            } else {
+                isDirected = false;
+            }
+        }
+
+        public void setWeighted(double weighted) {
+            if(weighted == 1) {
+                isWeighted = true;
+            } else {
+                isWeighted = false;
+            }
+        }
+
+        public boolean getIsDirected() {
+            return this.isDirected;
+        }
+
+		public boolean getIsWeighted() {
+            return this.isWeighted;
+        }
+
 	}
-	
-	// Check flag if graph is directed.
-	private static boolean isDirected(Double inputValue) {
-		if(inputValue == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	// Check flag if graph is weighted.
-	private static boolean isWeighted(Double inputValue) {
-		if(inputValue == 1) {
-			return true;
-		} else {
-			return false;
-		}
+
+	private static class Edge {
+
+        private int vertex;
+        private double weight;
+        private Edge next;
+
+        public Edge(int v, double w, Edge e) {
+            this.vertex = v;
+            this.weight = w;
+            this.next = e;
+        }
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		// Create scanner to read input .txt file with name txtFile
 		File file = new File("src/Input.txt");
-		Scanner txtFile = new Scanner(file);
-		
+        //This is the test case file from the Programming 1-2.pdf
+        //File file = new File("src/testCase.txt");
 
-		
+		Scanner txtFile = new Scanner(file);
+
 		while(txtFile.hasNextDouble()) {
 			inputArray.add(txtFile.nextDouble());
 		}
-		
+
 		// Now let's establish the properties of the graph, these will be passed to our build graph method.
-		boolean isDirected = isDirected(inputArray.get(2));
-		boolean isWeighted = isWeighted(inputArray.get(3));		
 		int numVertices = inputArray.get(0).intValue();
 		int numEdges = inputArray.get(1).intValue();
+
+		// Make a new graph object with the properties
+		Graph graph = new Graph(numVertices, numEdges);
+		graph.setDirected(inputArray.get(2));
+		graph.setWeighted(inputArray.get(3));
+
+        // Check if graph is weighted or unweighted, then build edges depending on this.
+        // Non-weighted graphs will just have edges created with 0 weight.
+        if(graph.getIsWeighted() == true) {
+
+            int vertexTracker = 4;
+            int edgeTracker = 5;
+            int weightTracker = 6;
+
+            // Undirected graphs need to have their vertex's added twice
+            if(!graph.getIsDirected()) {
+                 for(int i = 0; i < numEdges; i++) {
+                    addEdgeWeighted(graph, inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue(), inputArray.get(weightTracker));
+                    addEdgeWeighted(graph, inputArray.get(edgeTracker).intValue(), inputArray.get(vertexTracker).intValue(), inputArray.get(weightTracker));
+                    vertexTracker = (vertexTracker + 3);
+                    edgeTracker = (edgeTracker + 3);
+                    weightTracker = (weightTracker + 3);
+                 }
+            } else {
+                for(int i = 0; i < numEdges; i++) {
+                    addEdgeWeighted(graph, inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue(), inputArray.get(weightTracker));
+                    vertexTracker = (vertexTracker + 3);
+                    edgeTracker = (edgeTracker + 3);
+                    weightTracker = (weightTracker + 3);
+                }
+            }
+
+
+        } else {
+
+            int vertexTracker = 4;
+            int edgeTracker = 5;
+
+            if(!graph.getIsDirected()) {
+                 for(int i = 0; i < numEdges; i++) {
+                     addEdgeUnweighted(graph, inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue());
+                     addEdgeUnweighted(graph, inputArray.get(edgeTracker).intValue(), inputArray.get(vertexTracker).intValue());
+                     vertexTracker = (vertexTracker + 2);
+                     edgeTracker = (edgeTracker + 2);
+                 }
+            } else {
+                for(int i = 0; i < numEdges; i++) {
+                    addEdgeUnweighted(graph, inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue());
+                    vertexTracker = (vertexTracker + 2);
+                    edgeTracker = (edgeTracker + 2);
+                }
+            }
+
+
+        }
+		printGraph(graph);
 		
-		initializeGraph(numVertices);
-		
-		// Check if graph is weighted or unweighted, then build edges depending on this. 
-		// Non-weighted graphs will just have edges created with 0 weight.
-		if(isWeighted == true) {
-			int vertexTracker = 4;
-			int edgeTracker = 5;
-			int weightTracker = 6;
-			
-			
-			for(int i = 0; i < numEdges; i++) {
-				addEdgeWeighted(inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue(), inputArray.get(weightTracker));
-				vertexTracker = (vertexTracker + 3);
-				edgeTracker = (edgeTracker + 3);
-				weightTracker = (weightTracker + 3);
-			}
-		} else {
-			int vertexTracker = 4;
-			int edgeTracker = 5;
-			
-			for(int i = 0; i < numEdges; i++) {
-				addEdgeUnweighted(inputArray.get(vertexTracker).intValue(), inputArray.get(edgeTracker).intValue());
-				vertexTracker = (vertexTracker + 2);
-				edgeTracker = (edgeTracker + 2);
-			}
-			
-		}
-
-	}
-}
-
-
-// Edge objects
-class edge {
-	
-	int vertex;
-	double weight;
-	edge next;
-	
-	public edge(int v, double w, edge e) {
-		vertex = v;
-		weight = w;
-		next = e;
-	}
-	
-}
-
-// Graph objects
-class graph {
-	int numberOfVertices;
-	int numberOfEdges;
-	boolean directed;
-	
-	public graph(int v, int e, boolean d) {
-		numberOfVertices = v;
-		numberOfEdges = e;
-		directed = d;
+		BFS(graph);
 	}
 }
