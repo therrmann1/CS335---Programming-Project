7aff2b8b5b98f6b5bcda70a5d9984ecbcc451e06
Tyler Herrmann <nemo2924@live.com>
2018-11-19 16:25:08 -0600

    Added command line input, MST, and BFS edge classification

--- src/adjacencyList/AdjacencyList.java
+++ src/adjacencyList/AdjacencyList.java
@@ -2,6 +2,8 @@ package adjacencyList;
 
 import java.util.*;
 import java.io.FileNotFoundException;
+import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.io.File;
 
 public class AdjacencyList {
@@ -18,7 +20,7 @@ public class AdjacencyList {
         Edge next = null;
 
         // Create a new edge using the passed in values and giving it a null next value
-        Edge newEdge = new Edge(dest, weight, next);
+        Edge newEdge = new Edge(dest, src, weight, next);
 
         // If this edge is not the first then assign the previous edges next to the newly created edge.
         if(!graph.adjListArray[src].isEmpty()) {
@@ -36,7 +38,7 @@ public class AdjacencyList {
         Edge next = null;
 
         // Create a new edge using the passed in values and giving it a null next value and a 0 for weight
-        Edge newEdge = new Edge(dest, 0, next);
+        Edge newEdge = new Edge(dest, src, 0, next);
 
         // If this edge is not the first then assign the previous edges next to the newly created edge.
         if(!graph.adjListArray[src].isEmpty()) {
@@ -68,7 +70,11 @@ public class AdjacencyList {
 
             System.out.println("\n");
         }
-
+	    
+        try {
+			AdjacencyList.main(null);
+		} catch (FileNotFoundException e) {
+		}
 
 	}
 	
@@ -83,22 +89,63 @@ public class AdjacencyList {
 		}
 		
 		BFSrec(graph, discovered, processed, parent);
+		
+        try {
+			AdjacencyList.main(null);
+		} catch (FileNotFoundException e) {
+		}
 	}
 	
 	// Printing a BFS style representation of our graph.
 	private static void BFSrec(Graph graph, boolean discovered[], boolean processed[], int parent[]) {
-		PriorityQueue q = new PriorityQueue();
+		LinkedList q = new LinkedList();
+		LinkedList tempQ = new LinkedList();
+		int[] depth = new int[graph.numberOfVertices];
 		int v;
 		int y;
 		ArrayList<Edge> p = new ArrayList<Edge>();
+		boolean found[] = new boolean[graph.adjListArray.length];
 		
-		for(int i=0; i<graph.getNumberOfVertices();i++) {
-			q.add(i);
+		// Get the number of edges that are connected to the currently processing vertex
+		int currentVertex = 0;
+		int e = graph.adjListArray[0].size();
+		
+		tempQ.add(0);
+		q.add(0);
+		found[0] = true;
+		
+		while(q.size() != graph.adjListArray.length) {
+			for(int i=0; i<e; i++) {
+				if(!q.contains(graph.adjListArray[currentVertex].get(i).vertex)) {
+					if(graph.adjListArray[currentVertex].get(i).vertex != 0) {
+						q.add(graph.adjListArray[currentVertex].get(i).vertex);
+						tempQ.add(graph.adjListArray[currentVertex].get(i).vertex);
+						found[graph.adjListArray[currentVertex].get(i).vertex] = true;
+					}
+				}			
+			}
+			
+			if(!tempQ.isEmpty()) {
+				currentVertex = (int) tempQ.remove();
+				e = graph.adjListArray[currentVertex].size();
+			} else {
+				for(int i=0; i != graph.adjListArray.length; i++) {
+					if(found[i] == false) {
+						tempQ.add(i);
+						q.add(i);
+						currentVertex = i;
+						e = graph.adjListArray[currentVertex].size();
+						break;
+					}
+				}
+			}
+
 		}
 		
 		System.out.println("Print BFS:\n");
 		
 		while(!q.isEmpty()) {
+			
 			System.out.println("Process " + q.peek() + " early\n");
 			processed[(int)q.peek()] = true;
 			v = (int)q.remove();
@@ -111,18 +158,45 @@ public class AdjacencyList {
 			while(p.size() > 0) {
 				int counter = 0;
 				y = p.remove(counter).vertex;
-				
-				if((processed[y] == false) || graph.isDirected) {
-					System.out.println("Process edge " + v + " " + y + "\n");
-				} 
-				
+
 				if(discovered[y] == false) {
+					if(discovered[v] == false) {
+						discovered[v] = true;
+					}
 					discovered[y] = true;
+					depth[y] = depth[v] + 1;
 					parent[y] = v;
 				}
 				
+				if(!graph.isDirected) {
+					if(depth[y] - depth[v] >= 0) {
+						if(parent[y] == v) {
+							System.out.println("Process edge " + v + " " + y + " (Tree edge)\n");
+						} else if(!processed[y]) {
+							System.out.println("Process edge " + v + " " + y + " (Cross edge)\n");
+						}
+					}
+				}
+				
+				if(graph.isDirected) {
+					if(depth[y] - depth[v] >= 0) {
+						if(parent[y] == v) {
+							System.out.println("Process edge " + v + " " + y + " (Forward edge)\n");
+						} else if(!processed[y]) {
+							System.out.println("Process edge " + v + " " + y + " (Cross edge)\n");
+						}
+					} else {
+						System.out.println("Process edge " + v + " " + y + " (back edge)\n");
+					}
+				}
+
+				// Check if graph is directed or undirected and make sure to only have tree and cross edges for 
+				// undirected graphs. Do not use parent.
+				
 				counter++;
 			}
+			
+			
 			
 			System.out.println("Process " + v + " late\n");
 		}
@@ -143,6 +217,12 @@ public class AdjacencyList {
                 DFSrec(graph, i, visited, parent);
             }
         }
+        
+        try {
+			AdjacencyList.main(null);
+		} catch (FileNotFoundException e) {
+		}
+        
     }
 
     private static void DFSrec(Graph graph, int vertStart, boolean visited[], int parent[]) {
@@ -173,6 +253,108 @@ public class AdjacencyList {
         //process_vertex_late(vertStart);
 
     }
+    
+    private static void MST(Graph graph) {
+    	
+    	if(graph.isDirected || !graph.isWeighted) {
+    		System.out.println("kruskal's algorithm requires an undirected and weighted graph. The data given is either directed or unweighted. ");
+            
+    		try {
+    			AdjacencyList.main(null);
+    		} catch (FileNotFoundException e) {
+    		}
+    	} else {
+    		List<Edge> edges = new ArrayList<>();
+        	int edgeTracker = 0;
+    		Point p[] = new Point[graph.numberOfEdges]; 
+    		boolean duplicateEdge = false;
+        	
+        	for(int i=0; i != graph.adjListArray.length; i++) {
+        		for(int j=0; j != graph.adjListArray[i].size(); j++) {
+        			
+        			for(int k=0; k!=edgeTracker; k++) {
+        				if(p[k].x == i && p[k].y == graph.adjListArray[i].get(j).vertex) {
+        					duplicateEdge = true;
+        					break;
+        				}
+        			}
+        			
+        			if(!duplicateEdge) {
+        				edges.add(graph.adjListArray[i].get(j));
+            			p[edgeTracker] = new Point(graph.adjListArray[i].get(j).vertex, i);
+            			edgeTracker++;
+        			}
+        			duplicateEdge = false;
+        		}
+        	}
+        	        	
+        	Collections.sort(edges);
+        	
+    		List<Edge> mstEdges = new ArrayList<>();
+            int mstTracker = 0;
+            graph.numberOfEdges = 0;
+            int mstWeight = 0;
+    		
+            for(int i=0;i!=edges.size(); i++) {
+            	mstEdges.add(edges.get(i));
+            	graph.numberOfEdges++;
+            	
+            	if(isCycle(graph, mstEdges) == 1) {
+            		mstEdges.remove(mstTracker);
+            		graph.numberOfEdges--;
+            	} else {
+            		mstTracker++;
+            		mstWeight += edges.get(i).weight;
+            		System.out.println("Edge (" + edges.get(i).src+","+edges.get(i).vertex+") weight = " + edges.get(i).weight);
+            	}
+            }            
+             
+        	System.out.println("MST Cost = " + mstWeight);
+            try {
+    			AdjacencyList.main(null);
+    		} catch (FileNotFoundException e) {
+    		}
+    	}
+    	
+    }
+    
+    static int find(int parent[], int i) 
+    { 
+        if (parent[i] == -1) {
+        	return i; 
+        }  
+        return find(parent, parent[i]); 
+    } 
+
+    static void Union(int parent[], int x, int y) 
+    { 
+        int setX = find(parent, x); 
+        int setY = find(parent, y); 
+        parent[setX] = setY; 
+    } 
+  
+  
+    static int isCycle(Graph graph, List<Edge> edges) { 
+        int parent[] = new int[graph.numberOfVertices]; 
+  
+        for (int i=0; i<graph.numberOfVertices; ++i) {
+            parent[i]=-1;
+        }
+   
+        for (int i = 0; i < graph.numberOfEdges; ++i) 
+        { 
+            int x = find(parent, edges.get(i).src); 
+            int y = find(parent, edges.get(i).vertex); 
+  
+            if (x == y) {
+                return 1; 
+            }
+            
+            Union(parent, x, y); 
+        } 
+        return 0; 
+    } 
+    
 
 	private static class Graph {
 
@@ -234,16 +416,31 @@ public class AdjacencyList {
 
 	}
 
-	private static class Edge {
+	private static class Edge implements Comparable<Edge> {
 
         private int vertex;
+        private int src;
         private double weight;
         private Edge next;
+        
+	    @Override
+	    public int compareTo(Edge o) {
+	    double passedWeight = o.weight;
 
-        public Edge(int v, double w, Edge e) {
+	    if (weight == passedWeight)
+	        return 0;
+	    else if (weight > passedWeight)
+	        return 1;
+	    else
+	        return -1;
+	    }
+
+        public Edge(int v, int s, double w, Edge e) {
+        	this.src = s;
             this.vertex = v;
             this.weight = w;
             this.next = e;
+            
         }
 	}
 
@@ -251,8 +448,6 @@ public class AdjacencyList {
 
 		// Create scanner to read input .txt file with name txtFile
 		File file = new File("src/Input.txt");
-        //This is the test case file from the Programming 1-2.pdf
-        //File file = new File("src/testCase.txt");
 
 		Scanner txtFile = new Scanner(file);
 
@@ -319,10 +514,32 @@ public class AdjacencyList {
 
 
         }
+        
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Choose one of the following options: ");
+        System.out.println("1- Print the adjacency list");
+        System.out.println("2- BFS");
+        System.out.println("3- DFS");
+        System.out.println("4- MST");
+        System.out.println("5- Exit");
+        
+        int userSel = userIn.nextInt();
+        
+        if(userSel == 1) {
+    		printGraph(graph);
+        } else if(userSel == 2) {
+    		BFS(graph);
+        } else if(userSel == 3) {
+    		DFS(graph);
+        } else if(userSel == 4) {
+        	MST(graph);
+        } else if(userSel == 5) {
+        	System.exit(0);
+        } else {
+        	System.out.println("Invalid input please try again.");
+        }
+        
 
-		printGraph(graph);
-		BFS(graph);
-		DFS(graph);
-
+		userIn.close();
 	}
 }
